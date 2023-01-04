56d4e6aed3bd0cf3c314f178d3a63677c2bd0162
adam wong <adamwong246@gmail.com>
2023-01-04 06:06:35 -0800

    wipstash

--- src/TesterantoScheduler.ts
+++ src/TesterantoScheduler.ts
@@ -6,7 +6,7 @@ import { topologicalSort } from 'graphology-dag/topological-sort';
 import { TesterantoFeatures } from './Features';
 import { ITestJob, IT_FeatureNetwork } from './testShapes';
 
-const TIMEOUT = 100;
+const TIMEOUT = 1000;
 const OPEN_PORT = '';
 const testOutPath = "./dist/results/";
 const featureOutPath = "./dist/";
@@ -55,52 +55,7 @@ export class TesterantoScheduler {
   public launch() {
     setInterval(async () => {
       console.log(this.spinner(), this.queue.length, this.ports);
-      const qi = this.queue.pop();
-      if (!qi) {
-        console.log('feed me some tests plz')
-        return;
-      }
-      const { key, aborter, testResourceRequired, getCancellablePromise } = qi;
-
-      this.abort(key);
-
-      if (testResourceRequired === "na") {
-        this.jobs[key] = {
-          aborter,
-          cancellablePromise: getCancellablePromise({}).then(() => delete this.jobs[key])
-        }
-      }
-
-      if (testResourceRequired === "port") {
-        Object.values(this.ports).forEach((jobMaybe, portNumber) => {
-          if (jobMaybe && jobMaybe === key) {
-            this.ports[portNumber] = OPEN_PORT;
-          }
-        });
-
-        const foundOpenPort = Object.keys(this.ports).find((p) => this.ports[p] === OPEN_PORT);
-
-        if (foundOpenPort) {
-          const testPromise = getCancellablePromise({ port: foundOpenPort }).then(() => {
-            Object.keys(this.ports).forEach((p, k) => {
-              const jobExistsAndMatches = this.ports[p] === key;
-              if (jobExistsAndMatches) {
-                this.ports[p] = OPEN_PORT;
-              }
-            });
-
-            delete this.jobs[key]
-          });
-          this.ports[foundOpenPort] = key;
-          this.jobs[key] = {
-            aborter,
-            cancellablePromise: testPromise
-          };
-        } else {
-          console.log(`no port was open so send the ${key} job to the back of the queue`)
-          this.queue.push(qi);
-        }
-      }
+      this.pop();
     }, TIMEOUT);
   }
 
@@ -108,7 +63,7 @@ export class TesterantoScheduler {
     if (hash !== this.testSrcMd5s[key]) {
       console.log("running", key);
       this.testSrcMd5s[key] = hash;
-      this.addTest(new (fresh(distFile, require)[className])()[0], key);
+      this.push(new (fresh(distFile, require)[className])()[0], key);
     }
   }
 
@@ -135,101 +90,6 @@ export class TesterantoScheduler {
       }
     );
     this.regenerateReports();
-  }
-
-  private async addTest(testJob: ITestJob, key) {
-    // eslint-disable-next-line no-async-promise-executor
-    const cancellablePromise = (allocatedTestResource) => cancelable(new Promise(async (resolve) => {
-      const result = {
-        test: testJob.test,
-        status: await testJob.runner(allocatedTestResource)
-      };
-
-      await fs.promises.mkdir(testOutPath, { recursive: true });
-
-      fs.writeFile(
-        `${testOutPath}${key}.json`,
-        JSON.stringify(testJob.test.toObj(), null, 2),
-
-        (err) => {
-          if (err) {
-            console.error(err);
-          }
-          resolve(result)
-        }
-      );
-
-      for (const [gNdx, g] of testJob.test.givens.entries()) {
-        for (const testArtifactKey of Object.keys(g.testArtifacts)) {
-          for (const [ndx, testArtifact] of g.testArtifacts[testArtifactKey].entries()) {
-            const artifactOutFolderPath = `${testOutPath}${key}/${gNdx}/${ndx}/`
-            const artifactOutPath = `${artifactOutFolderPath}/${testArtifactKey}.png`
-            await fs.promises.mkdir(artifactOutFolderPath, { recursive: true });
-            await fs.writeFile(
-              artifactOutPath,
-              testArtifact.binary,
-              (err) => {
-                if (err) {
-                  console.error(err);
-                }
-                resolve(result)
-              }
-            );
-          }
-        }
-      }
-      for await (const [gNdx, given] of result.test.givens.entries()) {
-        for await (const givenFeature of given.features) {
-          for await (const knownFeature of this.testerantoFeatures.features) {
-            if (!this.featureTestJoin[givenFeature.name]) {
-              this.featureTestJoin[givenFeature.name] = {};
-            }
-            if (givenFeature.name === knownFeature.name) {
-              this.featureTestJoin[givenFeature.name][given.name] = {
-                suite: result.test,
-                whens: given.whens.map((w) => w.name),
-                thens: given.thens.map((t) => t.name),
-                errors: given.error,
-              }
-            } else {
-              // delete this.featureTestJoin[givenFeature.name][given.name];
-            }
-          }
-        }
-      }
-
-      fs.writeFile(
-        `${testOutPath}featureTestJoin.json`,
-        JSON.stringify(
-          Object.keys(this.featureTestJoin).reduce((mm, featureKey) => {
-            mm[featureKey] = Object.keys(this.featureTestJoin[featureKey]).map((testKey) => {
-              const ranJob = this.featureTestJoin[featureKey][testKey];
-              return {
-                testKey: testKey,
-                name: ranJob.suite.name,
-                whens: ranJob.whens,
-                thens: ranJob.thens,
-                errors: ranJob.errors
-              }
-            });
-            return mm;
-          }, {})
-          , null, 2),
-        (err) => {
-          if (err) {
-            console.error(err);
-          }
-          resolve(result)
-        }
-      );
-      this.regenerateReports();
-    }));
-    this.queue.push({
-      key,
-      aborter: testJob.test.aborter,
-      getCancellablePromise: cancellablePromise,
-      testResourceRequired: testJob.testResource
-    });
   }
 
   private dumpNetworks = () => {
@@ -326,4 +186,152 @@ export class TesterantoScheduler {
     this.spinCycle = (this.spinCycle + 1) % this.spinAnimation.length;
     return this.spinAnimation[this.spinCycle]
   }
+
+  private push(testJob: ITestJob, key) {
+    this.queue.push({
+      key,
+      aborter: testJob.test.aborter,
+      getCancellablePromise: this.startJob(testJob, key),
+      testResourceRequired: testJob.testResource
+    });
+  }
+
+  private pop() {
+    const qi = this.queue.pop();
+    if (!qi) {
+      console.log('feed me some tests plz')
+      return;
+    }
+    const { key, aborter, testResourceRequired, getCancellablePromise } = qi;
+
+    this.abort(key);
+
+    if (testResourceRequired === "na") {
+      this.jobs[key] = {
+        aborter,
+        cancellablePromise: getCancellablePromise({}).then(() => delete this.jobs[key])
+      }
+    }
+
+    if (testResourceRequired === "port") {
+      Object.values(this.ports).forEach((jobMaybe, portNumber) => {
+        if (jobMaybe && jobMaybe === key) {
+          this.ports[portNumber] = OPEN_PORT;
+        }
+      });
+
+      const foundOpenPort = Object.keys(this.ports).find((p) => this.ports[p] === OPEN_PORT);
+
+      if (foundOpenPort) {
+        const testPromise = getCancellablePromise({ port: foundOpenPort }).then(() => {
+          Object.keys(this.ports).forEach((p, k) => {
+            const jobExistsAndMatches = this.ports[p] === key;
+            if (jobExistsAndMatches) {
+              this.ports[p] = OPEN_PORT;
+            }
+          });
+
+          delete this.jobs[key]
+        });
+        this.ports[foundOpenPort] = key;
+        this.jobs[key] = {
+          aborter,
+          cancellablePromise: testPromise
+        };
+      } else {
+        console.log(`no port was open so send the ${key} job to the back of the queue`)
+        this.queue.push(qi);
+      }
+    }
+  }
+
+  private startJob(testJob, key) {
+    // eslint-disable-next-line no-async-promise-executor
+    return (allocatedTestResource) => cancelable(new Promise(async (resolve) => {
+      const result = {
+        test: testJob.test,
+        status: await testJob.runner(allocatedTestResource)
+      };
+
+      await fs.promises.mkdir(testOutPath, { recursive: true });
+
+      fs.writeFile(
+        `${testOutPath}${key}.json`,
+        JSON.stringify(testJob.test.toObj(), null, 2),
+
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+          resolve(result)
+        }
+      );
+
+      for (const [gNdx, g] of testJob.test.givens.entries()) {
+        for (const testArtifactKey of Object.keys(g.testArtifacts)) {
+          for (const [ndx, testArtifact] of g.testArtifacts[testArtifactKey].entries()) {
+            const artifactOutFolderPath = `${testOutPath}${key}/${gNdx}/${ndx}/`
+            const artifactOutPath = `${artifactOutFolderPath}/${testArtifactKey}.png`
+            await fs.promises.mkdir(artifactOutFolderPath, { recursive: true });
+            await fs.writeFile(
+              artifactOutPath,
+              testArtifact.binary,
+              (err) => {
+                if (err) {
+                  console.error(err);
+                }
+                resolve(result)
+              }
+            );
+          }
+        }
+      }
+      for await (const [gNdx, given] of result.test.givens.entries()) {
+        for await (const givenFeature of given.features) {
+          for await (const knownFeature of this.testerantoFeatures.features) {
+            if (!this.featureTestJoin[givenFeature.name]) {
+              this.featureTestJoin[givenFeature.name] = {};
+            }
+            if (givenFeature.name === knownFeature.name) {
+              this.featureTestJoin[givenFeature.name][given.name] = {
+                suite: result.test,
+                whens: given.whens.map((w) => w.name),
+                thens: given.thens.map((t) => t.name),
+                errors: given.error,
+              }
+            } else {
+              // delete this.featureTestJoin[givenFeature.name][given.name];
+            }
+          }
+        }
+      }
+
+      fs.writeFile(
+        `${testOutPath}featureTestJoin.json`,
+        JSON.stringify(
+          Object.keys(this.featureTestJoin).reduce((mm, featureKey) => {
+            mm[featureKey] = Object.keys(this.featureTestJoin[featureKey]).map((testKey) => {
+              const ranJob = this.featureTestJoin[featureKey][testKey];
+              return {
+                testKey: testKey,
+                name: ranJob.suite.name,
+                whens: ranJob.whens,
+                thens: ranJob.thens,
+                errors: ranJob.errors
+              }
+            });
+            return mm;
+          }, {})
+          , null, 2),
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+          resolve(result)
+        }
+      );
+      this.regenerateReports();
+    }))
+  }
+
 }
