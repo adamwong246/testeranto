bd2725e9c31be97c636b6c2a0a1956e2d841ee94
adam wong <adamwong246@gmail.com>
2023-01-08 14:50:28 -0800

    idk

--- src/build.ts
+++ src/build.ts
@@ -4,32 +4,32 @@ const configFile = `${process.cwd()}/${process.argv[2]}`;
 
 console.log("build.ts configFile", configFile);
 
-import(configFile).then((configModule) => {
-  const tProject = new TesterantoProject(configModule.default[0], configModule.default[1], configModule.default[2])
-  console.log("build.ts tProject", tProject);
+// import(configFile).then((configModule) => {
+//   const tProject = new TesterantoProject(configModule.default[0], configModule.default[1], configModule.default[2])
+//   console.log("build.ts tProject", tProject);
 
-  import("../" + tProject.features).then(features => {
-    features.default.builder();
-    console.log("dynamicly exporting the features");
-  });
+//   import("../" + tProject.features).then(features => {
+//     features.default.builder();
+//     console.log("dynamicly exporting the features");
+//   });
 
-  tProject.tests.forEach(([key, sourcefile, className]) => {
+//   tProject.tests.forEach(([key, sourcefile, className]) => {
 
-    import("../" + sourcefile).then(testSuite => {
-      console.log("dynamicly exporting", key)
-      try {
-        new testSuite[className]()[0].builder(sourcefile, tProject.features)
-      } catch (e) {
-        console.error(className);
-        console.error(testSuite[className].toString());
-        console.error(e);
-      }
+//     import("../" + sourcefile).then(testSuite => {
+//       console.log("dynamicly exporting", key)
+//       try {
+//         new testSuite[className]()[0].builder(sourcefile, tProject.features)
+//       } catch (e) {
+//         console.error(className);
+//         console.error(testSuite[className].toString());
+//         console.error(e);
+//       }
 
-    });
-  })
+//     });
+//   })
 
-  tProject.builder()
-})
+//   tProject.builder()
+// })
 
 
 export { }
