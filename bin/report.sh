22204be492d26c50242928f47dca805816b6e60d
adam wong <adamwong246@gmail.com>
2023-01-08 15:58:59 -0800

    fic boring script

--- bin/report.sh
+++ bin/report.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 # yarn nodemon --config nodemon.json ./node_modules/testeranto/src/build.ts testeranto.config.json
-cp ./node_modules/testeranto/dist/report.css ./dist/report.css
-cp ./node_modules/testeranto/dist/report.js ./dist/report.js
-cp ./node_modules/testeranto/dist/report.html ./dist/report.html
+cp ./node_modules/testeranto/dist/reporter.css ./dist/reporter.css
+cp ./node_modules/testeranto/dist/reporter.js ./dist/reporter.js
+cp ./node_modules/testeranto/dist/reporter.html ./dist/reporter.html
