d667f34a16962de30d7add65230cfee3bed61f40
adam wong <adamwong246@gmail.com>
2023-01-08 16:34:04 -0800

    idk

--- src/build.ts
+++ src/build.ts
@@ -9,7 +9,7 @@ console.log("build.ts configFile", configFile);
 import(configFile).then((configModule) => {
 
   const tProject = new TesterantoProject(configModule.default[0], configModule.default[1], configModule.default[2])
-  console.log("build.ts tProject", tProject);
+  // console.log("build.ts tProject", tProject);
 
   import(configFile).then(features => {
     features.default.builder();
@@ -18,7 +18,11 @@ import(configFile).then((configModule) => {
 
   tProject.tests.forEach(([key, sourcefile, className]) => {
 
-    import("../" + sourcefile).then(testSuite => {
+    const featureFile = process.cwd() + sourcefile;
+
+    console.log("build.ts featureFile", featureFile);
+
+    import(featureFile).then(testSuite => {
       console.log("dynamicly exporting", key)
       try {
         new testSuite[className]()[0].builder(sourcefile, tProject.features)
@@ -27,7 +31,6 @@ import(configFile).then((configModule) => {
         console.error(testSuite[className].toString());
         console.error(e);
       }
-
     });
   })
 
