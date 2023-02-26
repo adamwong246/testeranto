5768786747ab5bd40a771359a6773b5e78a0373d
adam wong <adamwong246@gmail.com>
2023-02-25 16:50:44 -0800

    allow custom config

--- bin/testeranto.sh
+++ bin/testeranto.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-ts-node  --swc --esm  ./node_modules/testeranto/src/testeranto.mts ./testeranto.config.mts
+ts-node  --swc --esm  ./node_modules/testeranto/src/testeranto.mts $1
