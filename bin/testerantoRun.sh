0c802158a0f9743aa3391a7dd1a725dfc0b240f1
adam wong <adamwong246@gmail.com>
2024-03-20 21:44:06 -0700

    stash

--- bin/testerantoRun.sh
+++ bin/testerantoRun.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
+# 
+# testerantoRun <outdir> <config_file>
+# example: 
+# testerantoRun js testeranto.test.mjs
 
-yarn tsc --module esnext --esModuleInterop --target esnext --moduleResolution node --outdir js; node $1
+yarn tsc --module esnext --esModuleInterop --target esnext --moduleResolution node --outdir $1; node $1/$2
