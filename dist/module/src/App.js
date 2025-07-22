fdd0a1144c634e6cebdd59c4c7e775ac9741e0ee
adam wong <adamwong246@gmail.com>
2025-07-22 12:58:27 -0700

    rebuild

--- dist/module/src/App.js
+++ dist/module/src/App.js
@@ -1,33 +1,56 @@
-import ReactDom from "react-dom/client";
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { ProjectsPage } from './ProjectsPage';
-import { ProjectPage } from './ProjectPage';
-import { TestPage } from './TestPage';
-import { BuildLogsPage } from './BuildLogsPage';
-import { Footer } from './Footer';
-export const App = () => {
-    const location = useLocation();
-    return (React.createElement(Router, null,
-        React.createElement("div", { className: "d-flex flex-column min-vh-100" },
-            React.createElement("nav", { className: "navbar navbar-expand-lg navbar-dark bg-dark" },
-                React.createElement("div", { className: "container-fluid" },
-                    React.createElement(Link, { className: "navbar-brand", to: "/" }, "Testeranto"),
-                    React.createElement("div", { className: "collapse navbar-collapse" },
-                        React.createElement("ul", { className: "navbar-nav me-auto" },
-                            React.createElement("li", { className: "nav-item" },
-                                React.createElement(Link, { className: "nav-link", to: "/" }, "Projects")))))),
-            React.createElement("main", { className: "flex-grow-1 p-3" },
-                React.createElement(Routes, null,
-                    React.createElement(Route, { path: "/", element: React.createElement(ProjectsPage, null) }),
-                    React.createElement(Route, { path: "/projects/:projectName", element: React.createElement(ProjectPage, null) }),
-                    React.createElement(Route, { path: "/projects/:projectName/tests/:testName", element: React.createElement(TestPage, null) }),
-                    React.createElement(Route, { path: "/projects/:projectName/builds/:runtime", element: React.createElement(BuildLogsPage, null) }))),
-            React.createElement(Footer, null))));
-};
-document.addEventListener("DOMContentLoaded", function () {
-    const elem = document.getElementById("root");
-    if (elem) {
-        ReactDom.createRoot(elem).render(React.createElement(App, {}));
-    }
-});
+"use strict";
+// import ReactDom from "react-dom/client";
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import { ProjectsPage } from './ProjectsPage';
+// import { ProjectPage } from './ProjectPage';
+// import { TestPage } from './TestPage';
+// import { BuildLogsPage } from './BuildLogsPage';
+// import { Footer } from './Footer';
+// import { ISummary, IBuiltConfig } from './Types';
+// export const App = () => {
+//   const location = useLocation();
+//   return (
+//     <Router>
+//       <div className="d-flex flex-column min-vh-100">
+//         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//           <div className="container-fluid">
+//             <Link className="navbar-brand" to="/">
+//               Testeranto
+//             </Link>
+//             <div className="collapse navbar-collapse">
+//               <ul className="navbar-nav me-auto">
+//                 <li className="nav-item">
+//                   <Link className="nav-link" to="/">
+//                     Projects
+//                   </Link>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav>
+//         <main className="flex-grow-1 p-3">
+//           <Routes>
+//             <Route path="/" element={<ProjectsPage />} />
+//             <Route path="/projects/:projectName" element={<ProjectPage />} />
+//             <Route
+//               path="/projects/:projectName/tests/:testName"
+//               element={<TestPage />}
+//             />
+//             <Route
+//               path="/projects/:projectName/builds/:runtime"
+//               element={<BuildLogsPage />}
+//             />
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// };
+// document.addEventListener("DOMContentLoaded", function () {
+//   const elem = document.getElementById("root");
+//   if (elem) {
+//     ReactDom.createRoot(elem).render(React.createElement(App, {}));
+//   }
+// });
--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,60 @@
-import ReactDom from "react-dom/client";
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { ProjectsPage } from './ProjectsPage';
-import { ProjectPage } from './ProjectPage';
-import { TestPage } from './TestPage';
-import { BuildLogsPage } from './BuildLogsPage';
-import { Footer } from './Footer';
-import { ISummary, IBuiltConfig } from './Types';
+// import ReactDom from "react-dom/client";
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import { ProjectsPage } from './ProjectsPage';
+// import { ProjectPage } from './ProjectPage';
+// import { TestPage } from './TestPage';
+// import { BuildLogsPage } from './BuildLogsPage';
+// import { Footer } from './Footer';
+// import { ISummary, IBuiltConfig } from './Types';
 
-export const App = () => {
-  const location = useLocation();
+// export const App = () => {
+//   const location = useLocation();
 
-  return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              Testeranto
-            </Link>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav me-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                    Projects
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+//   return (
+//     <Router>
+//       <div className="d-flex flex-column min-vh-100">
+//         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//           <div className="container-fluid">
+//             <Link className="navbar-brand" to="/">
+//               Testeranto
+//             </Link>
+//             <div className="collapse navbar-collapse">
+//               <ul className="navbar-nav me-auto">
+//                 <li className="nav-item">
+//                   <Link className="nav-link" to="/">
+//                     Projects
+//                   </Link>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav>
 
-        <main className="flex-grow-1 p-3">
-          <Routes>
-            <Route path="/" element={<ProjectsPage />} />
-            <Route path="/projects/:projectName" element={<ProjectPage />} />
-            <Route
-              path="/projects/:projectName/tests/:testName"
-              element={<TestPage />}
-            />
-            <Route
-              path="/projects/:projectName/builds/:runtime"
-              element={<BuildLogsPage />}
-            />
-          </Routes>
-        </main>
+//         <main className="flex-grow-1 p-3">
+//           <Routes>
+//             <Route path="/" element={<ProjectsPage />} />
+//             <Route path="/projects/:projectName" element={<ProjectPage />} />
+//             <Route
+//               path="/projects/:projectName/tests/:testName"
+//               element={<TestPage />}
+//             />
+//             <Route
+//               path="/projects/:projectName/builds/:runtime"
+//               element={<BuildLogsPage />}
+//             />
+//           </Routes>
+//         </main>
 
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// };
 
-document.addEventListener("DOMContentLoaded", function () {
-  const elem = document.getElementById("root");
-  if (elem) {
-    ReactDom.createRoot(elem).render(React.createElement(App, {}));
-  }
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const elem = document.getElementById("root");
+//   if (elem) {
+//     ReactDom.createRoot(elem).render(React.createElement(App, {}));
+//   }
+// });
