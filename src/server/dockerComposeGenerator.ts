383edb55f0ef2f27ffb5e42074b7e215bdce2d97
adam wong <adamwong246@gmail.com>
2025-12-07 10:06:40 -0800

    reorg

--- src/server/dockerComposeGenerator.ts
+++ src/server/dockerComposeGenerator.ts
@@ -4,10 +4,10 @@ import fs from "fs";
 import yaml from "js-yaml";
 import path from "path";
 import { IBuiltConfig, IRunTime } from "../../Types";
-import { webDocker } from "./webDocker";
-import { golangDocker } from "./golangDocker";
-import { nodeDocker } from "./nodeDocker";
-import { pythonDocker } from "./pythonDocker";
+import { webDocker } from "./web/webDocker";
+import { golangDocker } from "./golang/golangDocker";
+import { nodeDocker } from "./node/nodeDocker";
+import { pythonDocker } from "./python/pythonDocker";
 
 export async function setupDockerCompose(
   config: IBuiltConfig,
